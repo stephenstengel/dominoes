a7d02c25f6d8c890a37d7c578f9f62c6cbdd9121
RodriguezJe <49176383+RodriguezJe@users.noreply.github.com>
2019-04-10 08:31:53 -0700

    Update CRandom.java

--- CRandom.java
+++ CRandom.java
@@ -1,14 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *Project 1
  */
-package project1;
 
-/**
- *
- * @author RodriguezJo
- */
 public class CRandom {
 
     
