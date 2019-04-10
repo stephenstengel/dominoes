3830f1e5a53cc6e8ab572469c998eca9459d50b7
stephen <stephen.stengel@cwu.edu>
2019-04-10 11:23:11 -0700

    Added some comments.

--- Table.java
+++ Table.java
@@ -1,22 +1,34 @@
 /*
- * Project 1
+ * Table.java
  * 
- * This is the Class with the data structure that represents the board.
+ * This is the Class with the data structure that represents everything that is
+ * currently in the game including: players, player hands, the dominoes on the
+ * board.
  * 
  */
  
  import java.util.ArrayList;
  
 
-public class Table {
-	
-	//An arraylist filled with CDominoes objects.
+public class Table
+{
 	private ArrayList<Domino> dominoesOnTable;
+	
+	public Player player1;
+	public Player player2;
 	
 	
 	//constructor
 	public Table()
 	{
+		//make the dominoes
+		
+		//move first seven to player1.hand or whatever
+		
+		//move next seven to player2
+		
+		//keep remaining in dominoesOnTable
+		
 		
 	}
 	
