24c52db49e7efa197c62c4305f88a4825d9f0ba2
stephen <stephen.stengel@cwu.edu>
2019-04-10 14:06:31 -0700

    Added toString method, table printing method.

--- Table.java
+++ Table.java
@@ -34,9 +34,30 @@ public class Table
 	
 	
 	//Will print the current pieces on the table.
-	public void diplayTable()
+	public String toString()
 	{
+		//call player1.toString
 		
+		//player2.toString()
+		
+		//printTablePieces()
+		
+		return player1 + "\n" + player2 + "\n" + printTablePieces();
+	}
+	
+	
+	private ArrayList<Domino> createUnsortedDominoes()
+	{
+		ArrayList<Domino> unsortedDominoes = new ArrayList<Domino>();
+		
+		return unsortedDominoes;
+	}
+	
+	
+	//might need/want to make static later?
+	private String printTablePieces()
+	{
+		return dominoesOnTable.toString(); //change later
 	}
     
 }
