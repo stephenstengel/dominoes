eb3c98d3096b4c5f2f1ccf55ac0445640adf2d28
stephen <stephen.stengel@cwu.edu>
2019-04-10 08:08:23 -0700

    Fixed spelling in README. Added Dominoes.java file.

---
+++ Dominoes.java
@@ -0,0 +1,15 @@
+/*
+ * Dominoes Game
+ * 
+ * Copyright 2019 Stephen Stengel <stephen.stengel@cwu.edu>,
+ *                Jose Rodriquez
+ */
+
+
+public class Dominoes
+{
+	public static void main (String args[])
+	{
+		System.out.println("This is going to be a dominoes game!");
+	}
+}
