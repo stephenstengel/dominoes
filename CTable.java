9b8703858e1039c654ba0ca5a5e953ffe01036a3
stephen <stephen.stengel@cwu.edu>
2019-04-10 08:50:32 -0700

    Added an empty function to CTable.java

--- CTable.java
+++ CTable.java
@@ -4,7 +4,19 @@
  * This is the Class with the data structure that represents the board.
  * 
  */
+ 
+ import java.util.ArrayList;
+ 
 
 public class CTable {
+	
+	private ArrayList dominoesOnTable;
+	
+	
+	//Will print the current pieces on the table.
+	public void diplayTable()
+	{
+		
+	}
     
 }
