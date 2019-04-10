6a2925290f02aebda17a5ec9891338da6a9ee054
stephen <stephen.stengel@cwu.edu>
2019-04-10 12:58:40 -0700

    Added comments.

--- DominoGame.java
+++ DominoGame.java
@@ -11,5 +11,12 @@ public class DominoGame
 	public static void main (String args[])
 	{
 		System.out.println("This is going to be a dominoes game!");
+		
+		//create the table
+		
+		
+		//run the game
+		
+		
 	}
 }
