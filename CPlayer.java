b7736ee0cb4770c1afe34edf7d933e6cbff02344
stephen <stephen.stengel@cwu.edu>
2019-04-10 09:50:31 -0700

    Added framework to CPlayer

--- CPlayer.java
+++ CPlayer.java
@@ -1,7 +1,28 @@
 /*
  * Project 1
+ * 
+ * Will have data structure to hold the players hand.
+ * 
+ * Will have methods to display hand.
+ * 
  */
 
-public class CPlayer {
-    
+import java.util.ArrayList;
+
+public class CPlayer
+{
+	ArrayList<CDominoes> hand;
+	
+	//constructor
+	public CPlayer(ArrayList<CDominoes> inputHand)
+	{
+		hand = inputHand;
+	}
+	
+	
+	public void displayHand()
+	{
+		//iterate though arraylist and print each domino
+	}
+	
 }
