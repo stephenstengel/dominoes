6b3852fcdc852be54640c3de3d279faff31311cd
Stephen <stephen.stengel@cwu.edu>
2019-04-21 06:03:41 -0700

    Updated for new methods.

--- DominoGame.java
+++ DominoGame.java
@@ -24,19 +24,39 @@ public class DominoGame
 		
 		int winnerValue = myTable.playGame();
 		
+		System.out.println("\n\n##################################"
+							+ "\nBoard state: " + myTable.toString() );
 		
-		if (winnerValue == -1)
+		System.out.println("Value of winnerValue: " + winnerValue);
+		
+		if (winnerValue <= -1)
 		{
-			System.out.println("The game was a tie! The game was blocked and each"
-								+ " player has the same number of pips!");
-
-			//print number
+			System.out.println("The game was blocked by Player " + (winnerValue * -1) + "! Player with fewest dots wins!");
+			
+			System.out.println("Player 1 has: " + myTable.players[0].getNumberOfDots() + " dots!");
+			System.out.println("Player 2 has: " + myTable.players[1].getNumberOfDots() + " dots!");
+			
+			if ( myTable.players[0].getNumberOfDots() == myTable.players[1].getNumberOfDots() )
+			{
+				System.out.println("It's a tie! Each player has the same number of dots!");
+			}
+			else if ( myTable.players[0].getNumberOfDots() > myTable.players[1].getNumberOfDots() )
+			{
+				System.out.println("Player 2 wins!");
+			}
+			else
+			{
+				System.out.println("Player 1 wins!");
+			}
 		}
 		else if (winnerValue == 0 || winnerValue == 1)
 		{
 			System.out.println("The winner is Player" + (winnerValue + 1) + "!");
 		}
-		
+		else
+		{
+			System.out.println("ERROR in main!");
+		}
 		
 	}
 }
